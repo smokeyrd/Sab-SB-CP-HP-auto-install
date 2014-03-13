fb320f5f7bb1b8923c9f4e4a8f8c5ee66c2c1ad9
smokeyrd <rob@fauls.com>
2014-03-13 08:57:47 -0400

    Update Install-newuser.sh

--- Install-newuser.sh
+++ Install-newuser.sh
@@ -33,7 +33,7 @@ passentry()
    read userpass
    echo "Re-type the password"
    read retyped
-   if [[ $userpass == "$retyped" ]] ; then finish ; fi
+   if [[ $userpass == $retyped ]] ; then finish ; fi
    else passentry()
 }
 
